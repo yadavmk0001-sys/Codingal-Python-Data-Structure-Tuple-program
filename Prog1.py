d3f3335a83f754a36dd3a3917a9a0115ea3bd85b
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-06-09 21:21:27 +0530

    Add files via upload

---
+++ Prog1.py
@@ -0,0 +1,17 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50, 10, 50, 60,50, 70, 50)
+print(tuple1.count(50))
+
+tuple2 = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+_slice = tuple2[3:6]
+print(_slice)
+_slice = tuple2[:7]
+print(_slice)
